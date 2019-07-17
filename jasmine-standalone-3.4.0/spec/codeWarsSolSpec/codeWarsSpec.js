c7e559568f290231af299ac5566c8aae1fb33077
Kenneth Mahlatse Mokhethi <kennethmahlatsemokhethi@gmail.com>
2019-07-17 14:40:20 +0200

    Implemented a function that extract numbers from a string in the sortWordsWithNum.js file

---
+++ jasmine-standalone-3.4.0/spec/codeWarsSolSpec/codeWarsSpec.js
@@ -0,0 +1,12 @@
+describe("Code wars solution",()=>{
+   it("return numbers in a string",()=>{
+     var strW="hellow3 ever4y 1one";
+     var numStr=sortWords(strW);
+
+     expect(numStr).toBe(341);
+     
+
+   })
+
+
+})
--- jasmine-standalone-3.4.0/src/codeWarsSoljs/sortWordsWithNum.js
+++ jasmine-standalone-3.4.0/src/codeWarsSoljs/sortWordsWithNum.js
@@ -1,8 +1,22 @@
 function sortWords(words){
 
+    var arrNums=[];
+    var arrIndex=[];
 
-    var num=/\d+/g;
+
     
+        var num=/\d+/g;
+        //iterating through each element in
+        for(let chArrNums of arrNums){
+            if(chArrNums==num){
+                arrNums.push(chArrNums);
+            }
+        }
+
+    
+    
+
+    return arrNums;
     
 
 
