060727ae0c8891740ab0fff86a8e3f7cbb8121bf
Kenneth Mahlatse Mokhethi <kennethmahlatsemokhethi@gmail.com>
2019-07-29 11:43:18 +0200

    Extracted numbers from strings

--- jasmine-standalone-3.4.0/spec/codeWarsSolSpec/codeWarsSpec.js
+++ jasmine-standalone-3.4.0/spec/codeWarsSolSpec/codeWarsSpec.js
@@ -1,9 +1,9 @@
 describe("Code wars solution",()=>{
-   it("return numbers in a string",()=>{
+   fit("return numbers in a string",()=>{
      var strW="hellow3 ever4y 1one";
      var numStr=sortWords(strW);
 
-     expect(numStr).toBe(341);
+     expect(numStr).toEqual(['3','4','1']);
      
 
    })
--- jasmine-standalone-3.4.0/src/codeWarsSoljs/sortWordsWithNum.js
+++ jasmine-standalone-3.4.0/src/codeWarsSoljs/sortWordsWithNum.js
@@ -1,17 +1,13 @@
 function sortWords(words){
 
-    var arrNums=[];
-    var arrIndex=[];
+   
 
 
     
         var num=/\d+/g;
+        
         //iterating through each element in
-        for(let chArrNums of arrNums){
-            if(chArrNums==num){
-                arrNums.push(chArrNums);
-            }
-        }
+        var arrNums=words.match(num);
 
     
     
