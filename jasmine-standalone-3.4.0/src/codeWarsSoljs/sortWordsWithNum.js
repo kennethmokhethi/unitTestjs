aaace793c7d8b9728714944dd723ae2f46659faf
Kenneth Mahlatse Mokhethi <kennethmahlatsemokhethi@gmail.com>
2019-07-29 14:06:43 +0200

    Implemented the sort words.Incomplete

--- jasmine-standalone-3.4.0/src/codeWarsSoljs/sortWordsWithNum.js
+++ jasmine-standalone-3.4.0/src/codeWarsSoljs/sortWordsWithNum.js
@@ -1,13 +1,20 @@
 function sortWords(words){
 
-   
+       //Changing the string to a word array
+       var arrWords=words.split(" ");
 
+        for(let a=0;a<arrWords.length;a++)
+        {
+           //Extracting numbers from each element in the strings
+            var num=/\d+/g;   
+             for(let charArr of arrWords[a])
+             {
+                 
+               
+             };
+        };
 
-    
-        var num=/\d+/g;
-        
-        //iterating through each element in
-        var arrNums=words.match(num);
+     
 
     
     
